f14899b78ecd04d9864b3f958a8e480a1d2ae8b2
DavidAmmeraal <david.ammeraal@gmail.com>
2016-02-23 21:27:26 +0100

    Changed ip for testing

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 var prompt = require('prompt');
+var request = require('request');
 
+var authStr = "Basic " + new Buffer("david:JFvZ8F*M20^A9b").toString("base64");
+
+var doPost = function(userId, next){
+  request({
+    url: 'http://192.168.2.11:3000/api/consumptions', //URL to hit
+    method: 'POST',
+    headers: {
+        'Content-Type': 'text/json',
+        'Custom-Header': 'Custom Value',
+        'Authorization' : authStr
+    },
+    body: JSON.stringify({user: userId}) //Set the body as a string
+}, function(error, response, body){
+    if(error) {
+      console.log("HOI!");
+        console.log(error);
+    } else {
+        console.log("ADDED CONSUMPTION!");
+        next();
+    }
+});
+};
 
 var properties = [{
     description: 'Enter your Burcht User ID',     // Prompt displayed to the user. If not supplied name will be used.
@@ -8,14 +31,13 @@ var properties = [{
     message: 'Must be valid Burcht User ID', // Warning message to display if validation fails.
     required: true                        // If true, value entered must be non-empty.
   }];
+
 var requestInput = function(){
   prompt.get(properties, function(err, result) {
     if (err) {
       return onErr(err);
     }
-    console.log('Command-line input received:');
-    console.log('  User ID:: ' , result);
-    requestInput();
+    doPost(result.question, requestInput);
   });
 };
 
