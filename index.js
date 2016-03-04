0d3baa3265885a8c6c695b0946b5b77c5e8b9590
DavidAmmeraal <david.ammeraal@gmail.com>
2016-03-04 20:45:43 +0100

    works with latest burch-bar version

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ var prompt = require('prompt');
 var request = require('request');
 var jsonfile = require('jsonfile');
 
-var authStr = "Basic " + new Buffer("david:JFvZ8F*M20^A9b").toString("base64");
+var user;
+var password;
+
+jsonfile.readFile('conf.json', function(err, conf){
+  user = conf.user;
+  password = conf.password;
+});
+
+
 var file = 'open-consumptions.json';
 
 var doPost = function(data) {
+  var authStr = "Basic " + new Buffer(user + ":" + password).toString("base64");
   request({
-    url: 'http://192.168.2.11:3000/api/consumptions', //URL to hit
+    url: 'https://burcht.davidammeraal.nl/api/consumptions', //URL to hit
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -36,7 +45,7 @@ var doPost = function(data) {
 var properties = [{
   description: 'Enter your Burcht User ID', // Prompt displayed to the user. If not supplied name will be used.
   type: 'string', // Specify the type of input to expect.
-  pattern: /^[a-f\d]{24}$/,
+  pattern: /^[0-9]{4}$/,
   message: 'Must be valid Burcht User ID', // Warning message to display if validation fails.
   required: true // If true, value entered must be non-empty.
 }];
